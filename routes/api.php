2f3b42a8c46752242cc48eb0ca90704c8b202c55
jacob <jbs321.work@gmail.com>
2017-06-14 00:22:24 -0700

    add the cors middleware

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource("buildings", ['middleware' => 'cors', "BuildingController"]);
+Route::resource("buildings", "BuildingController")->middleware(\App\Http\Middleware\Cors::class);;
 Route::resource("users", "UsersController");
 
