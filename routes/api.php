102f0abb52bae8af29265a89f7f37e8f85008ee9
JB <jacob.lanir@grouphealth.ca>
2017-07-06 14:30:50 -0700

    add curl gazzel to code

--- routes/api.php
+++ routes/api.php
@@ -20,4 +20,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::resource("buildings", "BuildingController");
 Route::resource("users", "UsersController");
+Route::get("google", "GoogleController@index");
 
