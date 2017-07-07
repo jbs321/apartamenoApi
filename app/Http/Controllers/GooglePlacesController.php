e1008a00d26583587a6ef51aebc24e0767f03201
JB <jacob.lanir@grouphealth.ca>
2017-07-06 20:53:27 -0700

    add controllers

---
+++ app/Http/Controllers/GooglePlacesController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use GuzzleHttp\Client;
+use Illuminate\Http\JsonResponse;
+
+class GooglePlacesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+	    return new JsonResponse(["adasdas"]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($query = "")
+    {
+    	if($query != "") {
+		    $client   = new Client();
+		    $baseUri  = env("APP_GOOGLE_PLACES_BASE_URL");
+		    $appKey   = env("APP_GOOGLE_PLACES_API_KEY");
+		    $response = $client->get("{$baseUri}?key={$appKey}&query={$query}")->getBody();
+		    $data     = json_decode($response, true);
+		    return new JsonResponse($data);
+	    } else {
+    		dd("sdfsd");
+    		return new JsonResponse([
+    			"results" => []
+		    ]);
+	    }
+    }
+}
