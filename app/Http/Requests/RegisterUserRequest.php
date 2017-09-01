aefde3cd9e9c10453cc195a179bd42adfcfd461d
jacob <jbs321.work@gmail.com>
2017-09-01 09:28:05 -0700

    add new files

---
+++ app/Http/Requests/RegisterUserRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
+class RegisterUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return Auth::guest();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'email' => 'required|email|unique:users',
+            'address' => 'required',
+            'password' => 'required|confirmed',
+            'last_name' => 'required',
+            'first_name' => 'required',
+            'unit_number' => 'integer|min:1',
+            'phone_number' => 'required|max:30',
+        ];
+    }
+}
