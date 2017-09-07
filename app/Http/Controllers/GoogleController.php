c60164a26ec6a446aec4a7f0f4f9385ae43122c9
jlanir <jacob.lanir@grouphealth.ca>
2017-09-07 15:43:33 -0700

    fix google search

--- app/Http/Controllers/GoogleController.php
+++ app/Http/Controllers/GoogleController.php
@@ -5,29 +5,29 @@ namespace App\Http\Controllers;
 use App\Exceptions\NotFoundException;
 use Google\Facades\Google;
 use Google\Types\GooglePlacesResponse;
-use Illuminate\Http\JsonResponse;
 
-class GoogleController extends Controller
-{
-	public function showStreetViewImage($address = "",  $width = 640, $height = 250) {
-		$imageBinary = Google::streetView()->findImageByAddress($address, $width, $height);
-		return response($imageBinary)->header('Content-type', 'image/jpeg');
+class GoogleController extends Controller {
+	public function showStreetViewImage( $address = "", $width = 640, $height = 250 ) {
+		$imageBinary = Google::streetView()->findImageByAddress( $address, $width, $height );
+
+		return response( $imageBinary )->header( 'Content-type', 'image/jpeg' );
 	}
 
-	public function showStaticMapImage($address = "", $width = 640, $height = 640) {
-		$imageBinary = Google::staticMaps()->findImageByAddress($address, $width, $height);
-		return response($imageBinary)->header('Content-type', 'image/jpeg');
+	public function showStaticMapImage( $address = "", $width = 640, $height = 640 ) {
+		$imageBinary = Google::staticMaps()->findImageByAddress( $address, $width, $height );
+
+		return response( $imageBinary )->header( 'Content-type', 'image/jpeg' );
 	}
 
 	public function searchPlace( $query ) {
+
+		/** @var GooglePlacesResponse $result */
 		$result = Google::places()->findAddressByQuery( $query );
 
-		if ( $result->getStatus() !== GooglePlacesResponse::STATUS_TYPE__OK ) {
+		if ( ! $result->isFound() ) {
 			throw new NotFoundException();
 		}
 
-		$results = $result->toArray();
-
-		return $results;
+		return $result->toArray();
 	}
 }
